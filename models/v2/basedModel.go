fbc6a55fa38a213aa76ec99208485522c1c7ef9c
Evan Tantono <devanrt140999@gmail.com>
2024-11-19 13:37:31 +0700

    update v2 model Soft Delete

--- models/v2/basedModel.go
+++ models/v2/basedModel.go
@@ -8,15 +8,15 @@ import (
 
 type (
 	BaseModel struct {
-		CreatedAt time.Time `json:"createdAt"`
-		CreatedBy string    `json:"createdBy"`
-		UpdatedAt time.Time `json:"updatedAt"`
-		UpdatedBy string    `json:"updatedBy"`
+		CreatedAt time.Time `json:"createdAt" gorm:"column:created_at"`
+		CreatedBy string    `json:"createdBy" gorm:"column:created_by"`
+		UpdatedAt time.Time `json:"updatedAt" gorm:"column:updated_at"`
+		UpdatedBy string    `json:"updatedBy" gorm:"column:updated_by"`
 	}
 
 	BaseModelSoftDelete struct {
 		BaseModel
-		DeletedAt gorm.DeletedAt `json:"deletedAt"`
-		DeletedBy *string        `json:"deletedBy"`
+		DeletedAt gorm.DeletedAt `json:"deleteAt" gorm:"column:deleted_at"`
+		DeletedBy *string        `json:"deleteBy" gorm:"column:deleted_by"`
 	}
 )
